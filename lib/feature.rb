92d6aa8fece55e6b7ab8736fe849a075d773106c
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-06-09 09:40:50 +0100

    Quote all paths on the command line, since elements may contain spaces

--- lib/feature.rb
+++ lib/feature.rb
@@ -49,22 +49,23 @@ class Zucchini::Feature
     feature_text = File.open("#{@path}/feature.zucchini").read.gsub(/\#.+[\z\n]?/,"").gsub(/\n/, "\\n")
     File.open("#{run_data_path}/feature.coffee", "w+") { |f| f.write("Zucchini.run('#{feature_text}')") }
 
-    cs_paths  = "#{zucchini_base_path}/lib/uia #{@path}/../support/screens"
-    cs_paths += " #{@path}/../support/lib" if File.exists?("#{@path}/../support/lib")
-    cs_paths += " #{run_data_path}/feature.coffee"
+    cs_paths  = "\"#{zucchini_base_path}/lib/uia\" \"#{@path}/../support/screens\""
+    cs_paths += " \"#{@path}/../support/lib\"" if File.exists?("#{@path}/../support/lib")
+    cs_paths += " \"#{run_data_path}/feature.coffee\""
     
-    `coffee -o #{run_data_path} -j #{run_data_path}/feature.js -c #{cs_paths}`
+    `coffee -o "#{run_data_path}" -j "#{run_data_path}/feature.js" -c #{cs_paths}`
   end
 
   def collect
     with_setup do      
-      `rm -rf #{run_data_path}/*`
+      `rm -rf "#{run_data_path}/*"`
       compile_js
     
       device_params = (@device[:name] == "iOS Simulator") ? "" : "-w #{@device[:udid]}"
       
       begin
-        out = `instruments #{device_params} -t #{@template} #{Zucchini::Config.app} -e UIASCRIPT #{run_data_path}/feature.js -e UIARESULTSPATH #{run_data_path} 2>&1`
+        # Quote all the paths since path elements may legitimately contain spaces.
+        out = `instruments #{device_params} -t "#{@template}" "#{Zucchini::Config.app}" -e UIASCRIPT "#{run_data_path}/feature.js" -e UIARESULTSPATH "#{run_data_path}" 2>&1`
         puts out
         # Hack. Instruments don't issue error return codes when JS exceptions occur
         raise "Instruments run error" if (out.match /JavaScript error/) || (out.match /Instruments\ .{0,5}\ Error\ :/ )
